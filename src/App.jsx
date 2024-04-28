87bd52e5ff0e7643194da343aeb6a8fd1663b457
Jennifer Good <goodjen1014@gmail.com>
2024-04-27 23:40:15 -0500

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import './App.css'
 
 function App() {
   
   return (
     <>
+    <Container className="vertical-center">
       <h1>Cootie Catcher</h1>
       <Box sx={{p:1}}>
         <Button sx={{width:150}}
-          variant="contained">Let's PLay!</Button>
+          variant="contained">Play Game</Button>
       </Box>
       <Box sx={{p:1}}>
         <Button sx={{width:150}}
         variant="outlined">Learn More</Button>
       </Box>
+      </Container>
     </>
   )
 }
